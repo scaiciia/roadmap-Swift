6349fb876a07970672aba76d650eb01a72b630ce
Santiago Caiciia Massello <scaiciia@gmail.com>
2024-01-10 12:49:38 -0300

    add scaiciia.swift

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/swift/scaiciia.swift
@@ -0,0 +1,64 @@
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+import Foundation
+
+// https://www.swift.org/
+
+//Comentario en una sola linea
+
+/*
+ Comentarios
+ en
+ multilinea
+*/
+
+//Variable
+var variable
+
+//Constante
+let constante
+
+// Tipos de datos primitivos
+
+    //Enteros
+    var entero: Int = 1
+
+    //Float
+    var flotante: Float = 1.5
+
+    //Double
+    var doble: Double = 1.54
+
+    //Booleano
+    var booleano: Bool = true
+
+    //caracter
+    var caracter: Character = "C"
+
+    //Cadena de caracteres
+    var candena: String = "Esta es una cadena"
+
+//Imprimir en terminal
+print("¡Hola, Swift!")
